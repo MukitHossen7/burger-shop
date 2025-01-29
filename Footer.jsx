82f4eef9ab42a57c57f8bf9bb530ad0d5aac5d3c
himel1625 <himelmia1625@gmail.com>
2025-01-30 00:14:12 +0600

    implement initial Footer component

--- Footer.jsx
+++ Footer.jsx
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return <div>this is a footer of this projects</div>;
-};
+  
+  const styles = {
+    footer: {
+      backgroundColor: '#333',
+      color: 'white',
+      textAlign: 'center',
+      padding: '10px 0',
+      fontSize: '14px',
+    },
+  };
 
+  return (
+    <footer style={styles.footer}>
+      <p>
+        &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+      </p>
+    </footer>
+  );
+
+};
 export default Footer;
