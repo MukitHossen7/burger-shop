7b215ef15e37e10211b89afa3c7c6c1c9585024e
himel1625 <himelmia1625@gmail.com>
2025-01-29 23:32:04 +0600

    Added Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
 
 function App() {
+  const headingStyle = {
+    color: 'green',
+    fontSize: '24px',
+    textAlign: 'center',
+  };
+
   return (
     <div>
       <h1>welcome to our website</h1>
+      <h1 style={headingStyle}>welcome to our website</h1>
     </div>
   );
 }
