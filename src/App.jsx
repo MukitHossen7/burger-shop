568b3bfea04b1dfa73fff1ecc46522451dd1ff87
ataurwd <ataurrahman24707@gmail.com>
2025-01-30 00:38:19 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div>
       <h1>welcome to our website</h1>
       <h1>welcome to ours website</h1>
+      <h1>welcome to ataurwd</h1>
       <div>
         <Footer />
       </div>
